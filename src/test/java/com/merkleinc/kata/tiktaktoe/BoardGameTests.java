90c01b3de5732290c79decc1eeba8a9e5d2d783d
m-pacareu <marcpacareu@gmail.com>
2020-08-03 10:18:44 +0100

    Refactors gridTest
    Grid test splited in widthSize and heightSize to avoid multiple assertions in one test.

--- src/test/java/com/merkleinc/kata/tiktaktoe/BoardGameTests.java
+++ src/test/java/com/merkleinc/kata/tiktaktoe/BoardGameTests.java
@@ -5,6 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
@@ -15,12 +19,22 @@ public class BoardGameTests {
      * a game has nine fields in a 3x3 grid
      */
     @Test
-    public void correctGridSize(){
+    public void heightSize(){
+
+        BoardGame boardGame = new BoardGame();
+
+        assertEquals(3,boardGame.getHeight());
+
+    }
+
+    @Test
+    public void widthSize(){
 
         BoardGame boardGame = new BoardGame();
 
         assertEquals(3, boardGame.getWith());
-        assertEquals(3,boardGame.getHeight());
 
     }
+
+
 }
