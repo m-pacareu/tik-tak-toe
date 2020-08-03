a0560c6c5480aeaf7d49d4990a6ddef453815ec8
m-pacareu <marcpacareu@gmail.com>
2020-08-03 08:38:54 +0100

    Initial commit
    First commit with project structure.

---
+++ src/main/java/com/merkleinc/kat/tiktaktoe/TikTakToeApplication.java
@@ -0,0 +1,13 @@
+package com.merkleinc.kat.tiktaktoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TikTakToeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TikTakToeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/merkleinc/kat/tiktaktoe/TikTakToeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.merkleinc.kat.tiktaktoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TikTakToeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
